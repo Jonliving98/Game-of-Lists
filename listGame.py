e463d0276414a8d2d5788c108c58cb200fe20eb1
Jonliving98 <88949190+Jonliving98@users.noreply.github.com>
2022-04-02 00:22:34 -0400

    Add files via upload

---
+++ listGame.py
@@ -0,0 +1,158 @@
+import  os, random, keyboard, time
+
+clear = lambda: os.system('cls')
+
+
+keys = ['w','s','esc']
+
+pressed = []
+
+"sws"
+
+score = 0
+
+barrier = []
+
+coins = []
+
+
+
+class Player:
+   char = 8
+   x_pos= 0
+   y_pos= 0
+
+   
+x = Player.x_pos
+y = Player.y_pos
+
+class Board:
+   def position(dict,x,y):
+      count = 0
+      clear()
+      del dict
+      dict = [['_','_','_','_','_','_'],['_','_','_','_','_','_'],['_','_','_','_','_','_'],['_','_','_','_','_','_'],['Score is: %s' %(score)]]
+      dict[x][y] = Player.char
+      for Barrier in barrier:
+         if Barrier.y < 0:
+            barrier.remove(Barrier)
+         else:
+            dict[Barrier.x][Barrier.y] = Barrier.char
+      for Coin in coins:
+         if Coin.y < 0:
+            coins.remove(Coin)
+         else:
+            dict[Coin.x][Coin.y] = Coin.char
+      while count <= 4:
+         print(*dict[count], sep=' ')
+         count += 1
+
+class press:
+    def __init__(self, key):
+        self.key = keyboard.add_hotkey(key, lambda: [pressed.append(key), print(key)]) 
+
+for key in keys:
+   press(key)
+
+
+   
+class Barrier:
+      def __init__(self):
+         self.x = random.randint(0,3)
+         self.y = 5
+         self.char = 1
+      
+class Coin:
+      def __init__(self):
+         self.x = random.randint(0,3)
+         self.y = 5
+         self.char = 0
+   
+class Collision:
+      def check(score,x,y):
+         for Barrier in barrier:
+            for Coin in coins:
+
+               c = Coin.x, Coin.y
+               b = Barrier.x, Barrier.y
+               p = x, y
+               if c == b:
+                  barrier.remove(Barrier)
+               elif b == p:
+                  print("Game Over!")
+                  exit()
+               elif c == p:
+                  scored = True
+                  return scored
+while True:                 
+   class Barrier:
+      def __init__(self):
+         self.x = random.randint(0,3)
+         self.y = 5
+         self.char = 1
+      
+   class Coin:
+      def __init__(self):
+         self.x = random.randint(0,3)
+         self.y = 5
+         self.char = 0
+   
+   while len(barrier) <= 2:
+      barrier.append(Barrier())
+      
+   while len(coins) <= 0:
+      coins.append(Coin())
+
+   
+   
+
+   Board.position(dict,x,y)
+   
+   
+   s = Collision.check(score,x,y) 
+   
+   if s == True:
+      score += 1
+   
+
+   for Barrier in barrier:
+      Barrier.y -= 1
+
+   for Coin in coins:
+      Coin.y -= 1
+   
+   
+   
+
+
+   if 'w' in pressed:
+      x -= 1
+      if x >= 4 or x < 0:
+         x = 0
+      pressed.clear()
+      #Board.position(dict,x,y)
+   
+   if 's' in pressed:
+      x += 1
+      if x >= 4 or x < 0:
+         x = 0
+      pressed.clear()
+      #Board.position(dict,x,y)
+   #elif key == 'a':
+   #   y -= 1
+   #   if y >= 3 or y < 0:
+   #      x = 0
+   #   Board.position(dict,x,y)
+   #elif key == 'd':
+   #   y += 1
+   #   if y >= 3 or y < 0:
+   #      y = 0
+   #   Board.position(dict,x,y)
+   if 'esc' in pressed:
+      
+      
+      print('Highscore is %d!' %(score), 'Goodbye', sep = '\n')
+      
+      break
+   Board.position(dict,x,y)
+   time.sleep(0.8)
