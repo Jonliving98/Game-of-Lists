48df72e9111a3d524b1cba1f201fafbb464a1ff3
Jonny <88949190+Jonliving98@users.noreply.github.com>
2022-04-02 00:33:04 -0400

    Update listGame.py

--- listGame.py
+++ listGame.py
@@ -41,41 +41,29 @@ class Board:
          print(*dict[count], sep=' ')
          count += 1
 
+class Collision:
+   def check(score,x,y):
+      for Barrier in barrier:
+         for Coin in coins:
+            c = Coin.x, Coin.y
+            b = Barrier.x, Barrier.y
+            p = x, y
+            if c == b:
+               barrier.remove(Barrier)
+            elif b == p:
+               print("Game Over!")
+               exit()
+            elif c == p:
+               scored = True
+               return scored
+         
 class press:
     def __init__(self, key):
         self.key = keyboard.add_hotkey(key, lambda: [pressed.append(key), print(key)]) 
 
 for key in keys:
    press(key)
-
-class Barrier:
-      def __init__(self):
-         self.x = random.randint(0,3)
-         self.y = 5
-         self.char = 1
-      
-class Coin:
-      def __init__(self):
-         self.x = random.randint(0,3)
-         self.y = 5
-         self.char = 0
    
-class Collision:
-      def check(score,x,y):
-         for Barrier in barrier:
-            for Coin in coins:
-
-               c = Coin.x, Coin.y
-               b = Barrier.x, Barrier.y
-               p = x, y
-               if c == b:
-                  barrier.remove(Barrier)
-               elif b == p:
-                  print("Game Over!")
-                  exit()
-               elif c == p:
-                  scored = True
-                  return scored
 while True:                 
    class Barrier:
       def __init__(self):
